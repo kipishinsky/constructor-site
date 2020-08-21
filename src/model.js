3e40ef1544d31f0f9700aed0b094a1636375b390
SS007 <rbkmoneyukk@bk.ru>
2020-08-21 15:57:14 +0400

    add blocks-classes logical

--- src/model.js
+++ src/model.js
@@ -1,28 +1,36 @@
-import png from './image/filevini.png'
+import image from './image/image.png'
+import {TextBlock, TitleBlock, ImageBlock, TextColumnsBlock, BorodaBlock} from './classes/blocks'
+
+
+const text = `
+ 'Мы изучили десятки рынков, чтобы вы легко нашли красивый и удобный вариант сайта, подходящий именно под ваши бизнес-задачи. Просто добавляйте и перетаскивайте элементы как вам нравится, наблюдая за изменениями в понятном визуальном редакторе, с которым создание впечатляющего сайта становится лёгкой и приятной забавой. Используйте инструменты для привлечения клиентов и продвижения сайта в поисковых системах. Развивайте свой бизнес с помощью чатов, обратных звонков, рассылок, социальных сетей и систем аналитики'
+`
 
 export const model = [
-	{
-		type: 'title', value: 'TEST TITLE', options: {
-			img: png,
-			tag: 'h2',
-			styles: 'background: green; color: white;text-align: center;'
-		}
-	},
-	{
-		type: 'text', value: 'lorem10tab', options: {
-			img: png,
-			styles: 'background: blue; color: yellow; text-align: center; padding: 5px;'
-		}
-	},
-	{
-		type: 'textColumns', value: [
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, labore voluptatibus! Eveniet, fugiat quaerat. Assumenda at dolore, esse facilis harum id impedit odit praesentium quis rem repellat repellendus voluptate voluptates.',
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, labore voluptatibus! Eveniet, fugiat quaerat. Assumenda at dolore, esse facilis harum id impedit odit praesentium quis rem repellat repellendus voluptate voluptates.',
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, labore voluptatibus! Eveniet, fugiat quaerat. Assumenda at dolore, esse facilis harum id impedit odit praesentium quis rem repellat repellendus voluptate voluptates.'
-		], options: {
-			img: png,
-			styles: 'padding: 1rem; color: red;'
-		}
-	}
-]
+	new TitleBlock('Бесплатный конструктор сайтов на чистом JavaScript!', {
+		tag: 'h2',
+		styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;'
+	}),
+	new ImageBlock(image, {
+		styles: 'padding: 2rem 0; display: flex; justify-content: center;',
+		alt: 'my image',
+		imageStyles: 'width: 500px; height: auto;'
+	}),
+	new TextColumnsBlock([
+		'Огромный выбор шаблонов, созданных дизайнерами.',
+		'Меняйте, редактируйте и добавляйте все что угодно.',
+		'Ваш сайт будет отлично смотреться на любом устройстве.'
+	], {
+		styles: 'padding: 2rem 0; color: #fff;background: linear-gradient(to bottom, #8e2de2, #4a00e0);font-weight: bold;'
+	}),
+	new TextBlock(text, {
+		styles: 'background: linear-gradient(to left, #f2994a, #f2c94c);font-weight: bold;padding: 1rem; text-align: center;'
+	}),
 
+	new BorodaBlock(image, {
+		styles: 'padding: 2rem 0; display: flex; justify-content: center;',
+		alt: 'my image',
+		imageStyles: 'width: 300px; height: auto;'
+	})
+
+]
